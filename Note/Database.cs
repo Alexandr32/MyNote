107bf54e4b779782774e5c483416768c7b39a0e1
Alexandr <alex3289.aa@gmail.com>
2019-01-30 20:42:57 +0500

    Мое приложение заметки на .net

---
+++ Note/Database.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Xml.Serialization;
+
+namespace Note
+{
+    // Класс для вопроса    
+    [Serializable]
+    public class Notes : INotifyPropertyChanged
+    {
+        // Название записки
+        string nameNote;
+        // Текст записи
+        string textNote;
+
+        // Дата создания записи
+        public DateTime DateTime { get; }
+
+        // Событие которое происходит при изменении TextBox    
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string NameNote
+        {
+            get => nameNote;
+            set 
+            {
+                nameNote = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NameNote"));
+            }
+        }
+
+        public string TextNote
+        {
+            get => textNote;
+            set
+            {
+                textNote = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TextNote"));
+            }
+        }
+
+        public Notes()
+        {
+            DateTime = DateTime.Now;
+        }
+
+        public Notes(string _nameNote, string _textNote)
+        {
+            nameNote = _nameNote;
+            textNote = _textNote;
+            DateTime = DateTime.Now;
+        }
+
+        // Привязка по свойсву текста
+        public Binding BindingNote
+        {
+            get
+            { 
+                Binding bindingNote = new Binding
+                {
+                    Source = this,
+                    Path = new PropertyPath("TextNote"),
+                    Mode = BindingMode.TwoWay
+                };
+
+                return bindingNote;
+            }
+        }
+
+        // Привязка по свойсву имени
+        public Binding BindingName
+        {
+            get
+            {
+                // Привязка имени
+                Binding bindingName = new Binding
+                {
+                    Source = this,
+                    Path = new PropertyPath("NameNote"),
+                    Mode = BindingMode.TwoWay
+                };
+
+                return bindingName;
+            }
+        }
+    }
+
+    class Database
+    {
+        // Названеи файлов
+        string fileName;
+
+        // Список записей
+        ObservableCollection<Notes> listNotes;
+
+        // Событие которое происходит при изменении TextBox
+        PropertyChangedEventHandler propertyChangedEventHandler;
+
+        // Свойство списка
+        public ObservableCollection<Notes> ListNotes
+        {
+            get { return listNotes; }
+        }
+
+        // Св-во имени
+        public string FileName
+        {
+            set { fileName = value; }
+            get { return fileName; }
+        }
+
+        // Конструктор
+        public Database(string _fileName)
+        {
+            fileName = _fileName;
+            listNotes = new ObservableCollection<Notes>();
+        }
+
+        // Сохранение в файл
+        public void Save()
+        {
+            // Класс для сериализации объектов типа List<Notes> в xml
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(ObservableCollection<Notes>));
+
+            // Создается поток для записи сериализованного списка
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+
+            // Сериализируем список вопросов
+            xmlFormat.Serialize(fStream, listNotes);
+            fStream.Close();
+        }
+
+        // Загрузка файла
+        public void Load()
+        {
+            if(File.Exists(fileName))
+            {
+                // Класс для сериализации объектов типа List<Notes> в xml
+                XmlSerializer xmlFormat = new XmlSerializer(typeof(ObservableCollection<Notes>));
+
+                // Создается поток для чтения сериализованного списка
+                Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+                // Присваиваем переменной десариализованные данные
+                listNotes = (ObservableCollection<Notes>)xmlFormat.Deserialize(fStream);
+                fStream.Close();
+            }
+        }
+        
+
+        // Уведомление о изменении коллекции
+        public void Ghanged(NotifyCollectionChangedEventHandler delegateGhanged, PropertyChangedEventHandler _propertyChangedEventHandler)
+        {
+            propertyChangedEventHandler = _propertyChangedEventHandler;
+
+            // Передаем событие которое сработает при изменении колекции
+            listNotes.CollectionChanged += delegateGhanged;
+
+            foreach(var item in listNotes)
+            {
+                // Передаем событие которое сработает при изменении свойства
+                item.PropertyChanged += _propertyChangedEventHandler;
+            }
+            
+        }
+
+        public void Add(Notes note)
+        {
+            // Передаем событие которое сработает при изменении свойства
+            note.PropertyChanged += propertyChangedEventHandler;
+            listNotes.Add(note);
+        }
+        
+        // Сортировка по имени
+        public void SortingName()
+        {
+            listNotes = new ObservableCollection<Notes>(listNotes.OrderBy(u => u.NameNote));
+        }
+
+        // Сортировка по дате создания
+        public void SortingDateTime()
+        {
+            listNotes = new ObservableCollection<Notes>(listNotes.OrderBy(u => u.DateTime));
+        }
+
+        public int Count() => listNotes.Count();
+
+        public void Remove(Notes note)
+        {
+            listNotes.Remove(note);
+        }
+        
+    }
+}
---
+++ Note/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Note
+{
+    public partial class MainWindow : Window
+    {
+        Database database;
+        // Отображает были ли внесены изменения
+        bool isEdited = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            database = new Database("MyNotes");
+            database.Load();
+          
+            textBoxNote.Background = Brushes.White;
+            
+            // Делает нективным поля ввода при отсутствии выбора записи
+            EnebleTextBox();
+
+            // Связываем listBox и коллекцию
+            listBoxNote.ItemsSource = database.ListNotes;
+
+            // Привязка Label в head к имени записи
+            Binding bindingName = new Binding
+            {
+                Source = textBoxName,
+                Path = new PropertyPath("Text"),
+                Mode = BindingMode.TwoWay
+            };
+
+            // Связывает данные
+            labelName.SetBinding(Label.ContentProperty, bindingName);
+
+            // Фиксация метода который должен сработать при изменении данных
+            database.Ghanged(Users_CollectionChanged, Item_PropertyChanged);
+        }
+
+        private void ListBoxNote_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUI();
+        }
+
+        // Обновление интерфейся и привязка данных
+        private void UpdateUI()
+        {
+            // Получение данных из listBox
+            Notes note = (Notes) listBoxNote.SelectedValue;
+
+            if (note != null)
+            {
+                // Связывает значение
+                textBoxName.SetBinding(TextBox.TextProperty, note.BindingName);
+                textBoxNote.SetBinding(TextBox.TextProperty, note.BindingNote);
+            }
+
+            EnebleTextBox();
+        }
+
+
+        // Делает нективным поля ввода при отсутствии выбора записи
+        private void EnebleTextBox()
+        {
+            if (database.Count() == 0 || listBoxNote.SelectedValue == null)
+            {
+                textBoxName.IsEnabled = false;
+                textBoxNote.IsEnabled = false;
+            }
+            else
+            {
+                textBoxName.IsEnabled = true;
+                textBoxNote.IsEnabled = true;
+            }
+        }
+
+        private void MenuItem_Click_Deleted(object sender, RoutedEventArgs e)
+        {
+            Notes note = (Notes)listBoxNote.SelectedValue;
+            MessageBoxResult result = MessageBox.Show("Удалить запись " + note.NameNote + "?", string.Empty, MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                database.Remove(note);
+                textBoxName.Text = string.Empty;
+                textBoxNote.Text = string.Empty;
+            }
+        }
+
+        private void Click_AddNote(object sender, RoutedEventArgs e)
+        {
+            Notes note = new Notes()
+            {
+                NameNote = "Новая запись",
+                TextNote = "Текст записи",
+            };
+
+            database.Add(note);            
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            database.Save();
+
+            head_status.Background = new SolidColorBrush(Color.FromRgb(15, 52, 153));
+            textStatusFile.Visibility = Visibility.Hidden;
+            isEdited = false;
+        }
+
+        // Контекстное меню при нажатии на клавиши
+        private void BtnSetting_Click(object sender, RoutedEventArgs e)
+        {
+            btnSetting.ContextMenu.IsOpen = true;
+        }
+
+        private void CloseApp_Clic(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        // Срабатывает если были изменения в списке записей
+        public void Users_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateUINotSaved();
+        }
+
+        // Свойство срабатывает при внесении изменении какого-либо свойства записи
+        private void Item_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            UpdateUINotSaved();
+        }
+
+        // Изменяет интерфейса при не сохраненном файле
+        private void UpdateUINotSaved()
+        {
+            // Создает красную полоску
+            head_status.Background = Brushes.Red;
+            // Выводит касное сообщение о не сохраненным файле
+            textStatusFile.Visibility = Visibility.Visible;
+            isEdited = true;
+        }
+
+        // Метод по сортировки имени
+        private void Sorting_Click(object sender, RoutedEventArgs e)
+        {
+            // Сортировка 
+            database.SortingName();
+            // Связывание данных с отсортированной коллекцией
+            listBoxNote.ItemsSource = database.ListNotes;
+        }
+
+        private void SortingDataTime_Click(object sender, RoutedEventArgs e)
+        {
+            // Сортировка 
+            database.SortingDateTime();
+            // Связывание данных с отсортированной коллекцией
+            listBoxNote.ItemsSource = database.ListNotes;
+        }
+
+        private void Head_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            head.ContextMenu.IsOpen = true;
+        }
+        
+        // Перетаскивает окно при нажатии на head
+        private void LabelName_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isEdited)
+            {
+                MessageBoxResult result = MessageBox.Show("Сохранить данные?", "Есть несохраненные данные", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Yes)
+                {
+                    database.Save();
+                }
+                else if (result == MessageBoxResult.No)
+                {
+                    
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
